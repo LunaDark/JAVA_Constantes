ec55f61062eab95460329aae68211f9839844b18
Cristina <cristinacat12@gmail.com>
2020-05-11 10:20:03 -0300

    Initial commit

---
+++ src/contantes/Contantes.java
@@ -0,0 +1,19 @@
+
+package contantes;
+
+public class Contantes {
+public static double PI = 3.1416;
+
+  
+    public static void main(String[] args) {
+        
+        //if(valor == nulo || valor == 0)
+        //string.isNullOrEmpty( string qualquer)
+        /*
+        if(!string.isnullorempty(nome))
+        if(!(string.isnullorempty(nome) && string.isnullorempty(nome))
+        
+        */
+    }
+    
+}
